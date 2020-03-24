b9fb82b2c4f237ead1ddd1a8f7fbcedd12ad3045
JS <liderja@naver.com>
2020-03-24 22:51:11 +0900

    finish Commit

--- Weather.js
+++ Weather.js
@@ -8,7 +8,9 @@ import { LinearGradient } from 'expo-linear-gradient';
 const weatherOptions = {
     Haze: {
         iconName : "weather-hail",
-        gradient : ["#4DA0B0","#D39D38"]
+        gradient : ["#4DA0B0","#D39D38"],
+        titls : "Haze",
+        subtitle : "When go"
     },
 
     Thunderstorm :{
@@ -20,24 +22,26 @@ const weatherOptions = {
         gradient : ["#4DA0B0","#D39D38"]
     },
     Rain: {
-        iconName : "weather-hail",
-        gradient : ["#4DA0B0","#D39D38"]
+        iconName : "weather-rainy",
+        gradient : ["#00C62B","#005BEA"]
     }, 
     Snow: {
-        iconName : "weather-hail",
-        gradient : ["#4DA0B0","#D39D38"]
+        iconName : "weather-snowy",
+        gradient : ["#7DE2FC","#B9B6E5"]
     }, 
     Atmosphere: {
         iconName : "weather-hail",
-        gradient : ["#4DA0B0","#D39D38"]
+        gradient : ["#89F7FE","#66A6FF"]
     }, 
     Clear: {
-        iconName : "weather-hail",
-        gradient : ["#4DA0B0","#D39D38"] 
+        iconName : "weather-sunny",
+        gradient : ["#FF7300","#FEF253"] ,
+        title : "Sunny",
+        subtitle : "Today Weather Sunny"
     }, 
     Clouds: {
-        iconName : "weather-hail",
-        gradient : ["#4DA0B0","#D39D38"]
+        iconName : "weather-cloudy",
+        gradient : ["#D7D2CC","#304352"]
     }, 
     Mist: {
         iconName : "weather-hail",
@@ -62,7 +66,10 @@ export default function Weather({ temp ,condition }) {
                 />
                 <Text style={styles.temp}>{temp}</Text>
             </View>
-            <View style={styles.halfContainer} />
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                        <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+            </View>
             </LinearGradient>
     )
 }
@@ -89,5 +96,20 @@ const styles = StyleSheet.create({
         flex : 1,
         justifyContent : "center",
         alignItems : "center"
+    },
+    title : {
+        color:"white",
+        fontSize : 44,
+        fontWeight : "300",
+        marginBottom : 10
+    },
+    subtitle:{
+        color:"white",
+        fontWeight : "600",
+        fontSize : 24
+    },
+    textContainer:{
+        paddingHorizontal : 20,
+        alignItems : "flex-start" 
     }
 })
